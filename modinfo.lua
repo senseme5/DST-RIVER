b3b0b507d482a07178217bc2ceb8acd36aeec68d
Dsorbitol <senseme5@naver.com>
2019-01-18 07:14:16 +0900

    icon
    sadfsdf

--- modinfo.lua
+++ modinfo.lua
@@ -1,7 +1,7 @@
 -- This information tells other players more about the mod
 name = "River"
 description = "Dark lord of the abyss"
-author = "Kuromaguro & Dondonroncondon"
+author = "Kuromaguro & Dondonrondoncon"
 version = "1.0" -- This is the version of the template. Change it to your own number.
 
 -- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
